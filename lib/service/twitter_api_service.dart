dad4065241114d4012cc477b98191052f98f3f2c
pr_Mais <hiimechi@gmail.com>
2020-12-07 15:38:24 +0300

    generate oauth_nonce key

--- lib/service/twitter_api_service.dart
+++ lib/service/twitter_api_service.dart
@@ -1,9 +1,9 @@
 import 'dart:convert';
 import 'dart:io';
+import 'dart:math';
 
 import 'package:ably_cryptocurrency/config_example.dart';
 import 'package:http/http.dart' as http;
-
 class TwitterAPIService {
   TwitterAPIService({this.queryTag});
 
@@ -22,10 +22,15 @@ class TwitterAPIService {
   getTweetsQuery() async {
     String query = _twitterSearchUri(queryTag).toString();
     String timestamp = (DateTime.now().toUtc().millisecondsSinceEpoch / 1000).toString();
-    String authHeader =
-        'OAuth oauth_consumer_key="$OAuthConsumerKey", oauth_nonce="$OAuthConsumerSercert", oauth_signature="OAUTH_SIGNATURE", oauth_signature_method="HMAC-SHA1", oauth_timestamp="$timestamp", oauth_token="$OAuthToken", oauth_version="1.0"';
 
-    http.Response response = await http.get('$query', headers: {HttpHeaders.authorizationHeader: '$authHeader'});
+    // generates oauth_nonce Base 64 encoded 32 bytes
+    final random = List<int>.generate(32, (i) => Random.secure().nextInt(256));
+    String encodedRandom = base64Encode(random);
+
+    String authHeader =
+        'OAuth oauth_consumer_key="$OAuthConsumerKey", oauth_nonce="$encodedRandom", oauth_signature="OAUTH_SIGNATURE", oauth_signature_method="HMAC-SHA1", oauth_timestamp="$timestamp", oauth_token="$OAuthToken", oauth_version="1.0"';
+
+    http.Response response = await http.get('$query', headers: {HttpHeaders.authorizationHeader: authHeader});
 
     final decodedResponse = json.decode(response.body);
 
